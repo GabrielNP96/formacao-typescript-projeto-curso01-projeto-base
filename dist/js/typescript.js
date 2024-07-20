621decf60fb260ccd62c09883832bba7600437ca
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-19 22:29:38 -0300

    Arrays e type alias

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -1,4 +1,14 @@
+//tipos primitivos
 var valor = 3000;
 var nome = 'G';
 var qualquer = '';
 qualquer = 12;
+//arrays
+var lista = [];
+lista.push(1);
+lista.push(22);
+var novaTransacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+};
--- src/typescript.ts
+++ src/typescript.ts
@@ -1,4 +1,25 @@
+//tipos primitivos
 let valor: number = 3000;
 let nome: string = 'G';
 let qualquer: any = '';
 qualquer = 12;
+
+
+//arrays
+const lista: number[] = [];
+lista.push(1);
+lista.push(22);
+
+//type alias
+type Transacao = {
+    tipoTransacao: string,
+    data: Date,
+    valor: number
+
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+}
